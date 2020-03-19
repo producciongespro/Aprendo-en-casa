1f8631260a47e59094c4dc13db75bf0a62fac685
lchaconc <luis.chacon.campos@gmail.com>
2020-03-19 11:42:55 -0600

    Imp integración webservice

--- src/componentes/Menu.js
+++ src/componentes/Menu.js
@@ -5,7 +5,9 @@ import filtrar from '../modulos/filtrar';
 
 function Menu(props) {
     const docentes = filtrar(props.array, "poblacion", "Docentes");
-    console.log(docentes);
+    const estudiantes = filtrar(props.array, "poblacion", "Estudiantes");
+    const familias = filtrar(props.array, "poblacion", "Familias");
+    
 
     return (
         <div className="container">
@@ -20,14 +22,16 @@ function Menu(props) {
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est possimus libero reprehenderit cum optio expedita.
                     </p>
                         
-                        {
-                            docentes &&
+                        {                            
                             docentes.map((item, i) => (                    
                                     <div key={"docentes"+i} className="alert alert-primary" role="alert">
                                         <a href={item.link} target="_blank" rel="noopener noreferrer">
                                                 <h6>{ item.nombre }</h6>
                                         </a>
-                                        <span>{ item.descripcion }</span>
+                                          {item.descripcion !== "null" &&
+                                                <span>{ item.descripcion }</span>
+                                          }
+                                        
                                     </div>                                                                    
                             ))
 
@@ -37,12 +41,44 @@ function Menu(props) {
 
                 <div className="col-sm-4">                    
                     <h4>Estudiantes</h4>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est possimus libero reprehenderit cum optio expedita dolorum natus aut commodi porro. Tempora vitae est officiis rem praesentium doloribus veritatis, molestias suscipit?
+                    <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est possimus libero reprehenderit cum optio expedita.
+                    </p>
+                        
+                        {                            
+                            estudiantes.map((item, i) => (                    
+                                    <div key={"docentes"+i} className="alert alert-primary" role="alert">
+                                        <a href={item.link} target="_blank" rel="noopener noreferrer">
+                                                <h6>{ item.nombre }</h6>
+                                        </a>
+                                        {item.descripcion !== "null" &&
+                                                <span>{ item.descripcion }</span>
+                                          }
+                                    </div>                                                                    
+                            ))
+
+                        }
                     </div>
 
                 <div className="col-sm-4">
                     <h4>Familia</h4>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est possimus libero reprehenderit cum optio expedita dolorum natus aut commodi porro. Tempora vitae est officiis rem praesentium doloribus veritatis, molestias suscipit?
+                    <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est possimus libero reprehenderit cum optio expedita.
+                    </p>
+                        
+                        {                           
+                            familias.map((item, i) => (                    
+                                    <div key={"docentes"+i} className="alert alert-primary" role="alert">
+                                        <a href={item.link} target="_blank" rel="noopener noreferrer">
+                                                <h6>{ item.nombre }</h6>
+                                        </a>
+                                        {item.descripcion !== "null" &&
+                                                <span>{ item.descripcion }</span>
+                                          }
+                                    </div>                                                                    
+                            ))
+
+                        }
                     </div>
             </div>
 
