591a759a0dac1dae90b2b1c98a15e1eb93e52de4
lchaconc <luis.chacon.campos@gmail.com>
2020-04-02 10:37:34 -0600

    Prueba Ajxax 2
    -Se implementa  ajax mediante Axios
    -Se imp ajax mediante API POST del form html

---
+++ Prueba-Login/login.php
@@ -0,0 +1,40 @@
+<?php
+$usuario = $_POST['usuario'];
+$pas =  $_POST['clave'];
+
+include "conectar.php";
+$mysqli = conectarDB();
+    //sleep(1);	
+  
+    
+  if ($nueva_consulta = $mysqli->prepare("SELECT 
+  usuarios.nombre, usuarios.clave, usuarios.apellido1, usuarios.apellido2, usuarios.usuario, usuarios.idTipoUsuario, usuarios.id, usuarios.departamento, tipo_usuario.etiquetaTipoUsuario, tipo_usuario.descripcionTipoUsuario 
+  FROM usuarios 
+  INNER JOIN tipo_usuario ON usuarios.idTipoUsuario = tipo_usuario.idTipoUsuario
+  WHERE usuario = ?")) {
+        $nueva_consulta->bind_param('s', $usuario);
+        $nueva_consulta->execute();
+        $resultado = $nueva_consulta->get_result();
+        if ($resultado->num_rows == 1) {
+            $datos = $resultado->fetch_assoc();
+             $encriptado_db = $datos['clave'];
+            if (password_verify($pas, $encriptado_db))
+            {
+                $_SESSION['usuario'] = $datos['usuario'];
+                echo json_encode(array('error'=>false,'usuario'=>$datos['usuario'], 'nombre'=>$datos['nombre'],  'apellido1'=>$datos['apellido1'],  'apellido2'=>$datos['apellido2'], 'id'=>$datos['id'], 'idTipoUsuario'=>$datos['idTipoUsuario'], 'etiquetaTipoUsuario'=>$datos['etiquetaTipoUsuario']  ) );
+              }
+
+               else {
+
+                 echo json_encode(array('error'=>true, 'error_msg' => 'La clave es incorrecta, vuelva a intentarlo.'));
+                    }
+        }
+        else {
+              echo json_encode(array('error'=>true, 'error_msg' => 'El usuario no existe.'));
+        }
+        $nueva_consulta->close();
+      }
+ // }
+$mysqli->close();
+
+?>
---
+++ Prueba-Login/login2.php
@@ -0,0 +1,11 @@
+<?php
+   header("Access-Control-Allow-Origin: *");
+  
+   $usuario =  $_POST['usuario'];
+   $clave = $_POST['clave'];   
+   echo $usuario;
+   echo $clave;
+
+  
+  
+  ?>
